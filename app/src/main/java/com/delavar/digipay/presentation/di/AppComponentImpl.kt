139c00fcb8579bf8499f8d3cd202d101fd4a5fa5
delavar <ali_delavar1390@yahoo.com>
2019-09-14 18:10:01 +0430

    add header token

--- app/src/main/java/com/delavar/digipay/presentation/di/AppComponentImpl.kt
+++ app/src/main/java/com/delavar/digipay/presentation/di/AppComponentImpl.kt
@@ -3,18 +3,32 @@ package com.delavar.digipay.presentation.di
 import android.app.Application
 import android.content.SharedPreferences
 import com.delavar.digipay.data.restful.ApiService
+import com.delavar.digipay.data.source.local.PrefSource
 import com.delavar.digipay.data.source.local.PreferenceHelper
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import com.delavar.digipay.data.source.local.PreferenceHelper.get
 
 class AppComponentImpl(val app: Application) : AppComponent {
 
     override val pref: SharedPreferences by lazy {
-        PreferenceHelper.customPrefs(app.applicationContext,"pref")
+        PreferenceHelper.customPrefs(app.applicationContext, "pref")
     }
 
     override val retrofit: Retrofit by lazy {
+        val client = OkHttpClient.Builder().addInterceptor {
+            val token: String? = pref[PrefSource.KEY_TOKEN]
+            var request = it.request()
+            token?.let {
+                request = request.newBuilder()
+                    .addHeader("Authorization", "Bearer $it")
+                    .build()
+            }
+
+            it.proceed(request)
+        }
         Retrofit.Builder().baseUrl("https://api.spotify.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
